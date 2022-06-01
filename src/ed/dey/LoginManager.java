ec33b076709a43842d6f48ca22a76960a853cd81
Eddeyy <theroosterstudios@gmail.com>
2022-06-01 23:01:07 +0200

    fixing previous commit

---
+++ src/ed/dey/LoginManager.java
@@ -0,0 +1,141 @@
+package ed.dey;
+
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LoginManager {
+    Connection con;
+    Scanner sc = new Scanner(System.in);
+    int u_id;
+    String dbURL = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+    String username = "DWELLER";
+    String password = "dwlr";
+
+    LoginManager(){
+        establishConnection();
+    }
+
+    void establishConnection()
+    {
+        try {
+            con = DriverManager.getConnection(dbURL, username, password);
+            System.out.println("Connected to Oracle database server as " + username + " successfully");
+        }
+            catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void adjustRole(){
+        String job_querry = "SELECT JOB_ID FROM LOGIN_DWELLERS_LIST WHERE DWELLER_ID = " + u_id;
+        try {
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(job_querry);
+            rs.next();
+            int job = Integer.parseInt(rs.getString("JOB_ID"));
+
+            switch(job){
+                case 1:
+                    con.close();
+                    con = DriverManager.getConnection(dbURL, "V_OVERSEER", "ovr");
+                    break;
+                case 2:
+                    con.close();
+                    con = DriverManager.getConnection(dbURL, "ENGINEER", "eng");
+                    break;
+                case 3:
+                    con.close();
+                    con = DriverManager.getConnection(dbURL, "MANAGER", "mgr");
+                    break;
+                case 4:
+                    con.close();
+                    con = DriverManager.getConnection(dbURL, "GUARD", "grd");
+                    break;
+                case 6:
+                    con.close();
+                    con = DriverManager.getConnection(dbURL, "MEDIC", "mdc");
+                    break;
+                default:
+                    con.close();
+                    con = DriverManager.getConnection(dbURL, "DWELLER", "dwlr");
+            }
+        }catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean login() throws SQLException, ParseException {
+        boolean success = false;
+        String[] parse;
+
+        System.out.print("\nEnter your NAME, SURNAME, and ID in a \"N S I\" format: ");
+
+        String command = sc.nextLine();
+
+        if(command.equalsIgnoreCase("QUIT")) {
+            Application.exit_success();
+        }
+
+        if(command.isEmpty()) {
+            throw new ParseException("Please type something.", 0);
+        }
+
+        parse = command.split(" ");
+
+        if(parse.length > 3) {
+            throw new ParseException("Provided too much information", command.indexOf(" ",command.indexOf(" ",command.indexOf(" "))));
+        }
+
+        if(parse.length < 3) {
+            throw new ParseException("Provided too little information", command.indexOf(" ",command.indexOf(" ",command.indexOf(" "))));
+        }
+
+        try{
+            u_id = Integer.parseInt(parse[2]);
+        }catch(NumberFormatException e){
+            throw new ParseException("Please make sure, the ID is a number", command.lastIndexOf(" "));
+        }
+
+        try {
+            return checkingName(con, parse[0], parse[1], u_id);
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean checkingName(Connection con, String name, String surname, int id) throws SQLException {
+        String query =
+                "SELECT * FROM LOGIN_DWELLERS_LIST WHERE NAME = '"+ name +"' AND SURNAME = '" + surname +"'";
+
+        try (Statement stmt = con.createStatement()) {
+            ResultSet rs = stmt.executeQuery(query);
+            rs.next();
+            int idCheck = rs.getInt("dweller_ID");
+            String nameCheck = rs.getString("name");
+            String surnameCheck = rs.getString("surname");
+            System.out.println(idCheck + ", " + nameCheck + ", " + surnameCheck);
+            System.out.println(id + ", " + name + ", " + surname);
+            if(idCheck == id && nameCheck.equals(name) && surnameCheck.equals(surname)){
+                System.out.println("Brawo debilu");
+                adjustRole();
+                return true;
+            }
+            else {
+                System.out.println("Jestes chujem");
+                return false;
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Oops, chuju");
+            Application.printSQLException(e);
+        }
+        return false;
+    }
+}
+
